ad700c2314ccd3fc757a64c46dcf2f231013f3e5
Stephen Malito <stevemalito@gmail.com>
2019-09-08 19:24:55 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import Header from './Components/Header'
+import './App.css';
+
+function App() {
+  return (
+   <>
+    <Header></Header>
+   </>
+  );
+}
+
+export default App;
